961c29d7095e55d4fa8d37bc91456cbfc02de67d
sunday781019 <sunday781019@gmail.com>
2015-09-19 17:56:17 +0800

    svdmsvnvfknbkfnlkvfkslvfffffffffffffuck

---
+++ Assets/Script/Platforms/BasePlatform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlatform : MonoBehaviour {
+
+	public enum Direction {
+		TOP, DOWN, RIGHT, LEFT
+	}
+	public enum PlatformType {
+		MOVE, ROTATION
+	}
+
+	protected void init() {
+		f_StartPosition_X = transform.position.x;
+		f_StartPosition_Y = transform.position.y;
+		f_EndPosition_X = f_StartPosition_X + f_MoveDistance;
+		f_EndPosition_Y = f_StartPosition_Y + f_MoveDistance;
+	}
+
+	// Main Parameters (Default)
+	public float i_MoveSpeed = 5; // Move Speed
+	public Direction m_Direction; // Direction
+	public PlatformType m_PlatformType { get; set; } // Platform Type
+	public float f_MoveDistance = 2; // 
+
+	protected float f_StartPosition_X;
+	protected float f_StartPosition_Y;
+	protected float f_EndPosition_X;
+	protected float f_EndPosition_Y;
+
+	// Control Parameters
+	public bool isShow;
+	public bool isEntity;
+
+}
+
+
---
+++ Assets/Script/Platforms/MovePlatform.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlatform : BasePlatform {
+
+	public MovePlatform(PlatformType type) {
+		this.m_PlatformType = type;
+	}
+	
+	void Awake() {
+		init();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (this.m_Direction) {
+			case Direction.TOP:
+				transform.position += Vector3.up * i_MoveSpeed * Time.deltaTime;
+				if(transform.position.y >= f_EndPosition_Y) {
+					changeDirection(Direction.DOWN);
+				}
+				break;
+			case Direction.DOWN:
+				transform.position -= Vector3.up * i_MoveSpeed * Time.deltaTime;
+				if(transform.position.y <= f_StartPosition_Y) {
+					changeDirection(Direction.TOP);
+				}
+				break;
+			case Direction.LEFT:
+				break;
+			case Direction.RIGHT:
+				break;
+		}
+	}
+	
+	// Change Direction
+	public void changeDirection(Direction newDirection) {
+		this.m_Direction = newDirection;
+	}
+}
